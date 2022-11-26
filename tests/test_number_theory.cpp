773571026167dd72d31a052efbb24f992a5caec4
Sumanth NR <sumanthnr62@gmail.com>
2022-11-26 23:16:51 +0530

    Updated Tests

--- tests/test_number_theory.cpp
+++ tests/test_number_theory.cpp
@@ -3,23 +3,32 @@
 
 using namespace Sig;
 
-#define MOD_INT_DEBUG 1
+#define NT_DEBUG 1
 
 int main() {
 
-#if MOD_INT_DEBUG == 0
+#if NT_DEBUG == 0
 	ModInt::M = 1e9 + 7;
-	Int n = 2000045;
+	Int n = 20000;
 	try {
 		auto x = quadraticResidueTonelliShanks(n);
-		cout << x << '\n';
-		cout << x * x << '\n';
+		std::cout << x << '\n';
+		std::cout << x * x << '\n';
 	} catch (const std::exception &e) {
-		cout << e.what() << '\n';
-		cout << ModInt::pow(n, (ModInt::M - 1) / 2) << '\n';
+		std::cout << e.what() << '\n';
+		std::cout << ModInt::pow(n, (ModInt::M - 1) / 2).getVal() - ModInt::M << '\n';
+	}
+	n = 200004;
+	try {
+		auto x = quadraticResidueTonelliShanks(n);
+		std::cout << x << '\n';
+		std::cout << x * x << '\n';
+	} catch (const std::exception &e) {
+		std::cout << e.what() << '\n';
+		std::cout << ModInt::pow(n, (ModInt::M - 1) / 2).getVal() - ModInt::M << '\n';
 	}
 
-#elif MOD_INT_DEBUG == 1
+#elif NT_DEBUG == 1
 	ModInt::M = 421;
 	Int counter = 0;
 	for (Int i = 1; i < ModInt::M; i++) {
---
+++ tests/test_poly_factor.cpp
@@ -0,0 +1,11 @@
+#include <number_theory.hpp>
+#include <polynomial.hpp>
+
+using namespace Sig;
+
+#define POLY_FACTOR_DEBUG 0
+
+int main() {
+
+	return 0;
+}
